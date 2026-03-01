255eff859a306650b38be4b5b4dace098d875212
Rose <kamdoum.rosine@yahoo.fr>
2026-03-01 17:36:18 -0500

    add new script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pwd
+ls -la
+echo "Hello World"
+echo "This is a sample script to demonstrate the use of a shell script in a Docker container."
+echo "You can add any commands you want to this script, and they will be executed when
